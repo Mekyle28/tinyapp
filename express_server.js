bbfbdfa7e052b631d71772d266037212fa0d0cd6
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-13 23:10:32 -0400

    add short URL redirect & add new shortURL and longURL pairs to database

--- express_server.js
+++ express_server.js
@@ -5,7 +5,7 @@ const PORT = 8080;
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
-const generateRandomString() => {
+const generateRandomString = function() {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let result = '';
   const charactersLength = characters.length;
@@ -14,7 +14,7 @@ const generateRandomString() => {
   }
   return result;
 }
-generateRandomString();
+
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -46,11 +46,19 @@ app.get('/urls/:id/', (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
 
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const tempShortULR = generateRandomString();
+  urlDatabase[tempShortULR] = req.body.longURL;
+  res.redirect(`/urls/${tempShortULR}`); // Respond with 'Ok' (we will replace this)
 });
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
