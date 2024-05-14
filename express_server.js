42d22c74d1ec4adfd6a1a4d36aef259a71de8c7e
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-13 22:32:51 -0400

    add random number function

--- express_server.js
+++ express_server.js
@@ -5,6 +5,16 @@ const PORT = 8080;
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
+const generateRandomString() => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  const charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+generateRandomString();
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -32,10 +42,15 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get('/urls/:id/', (req, res) => {
-  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
